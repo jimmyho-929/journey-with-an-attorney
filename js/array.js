fd21b363a2bb94bfcc9f6443ce226f254d6126be
Jimmy Ho <jimmyho.929@outlook.com>
2021-03-22 15:40:54 -0400

    testing commit

---
+++ js/array.js
@@ -0,0 +1,12 @@
+const places = [
+    {   place: "Boston, MA",
+        descr: " ",
+        img: "img/boston.jpg"
+        },
+    {   place: "Denver, CO",
+        descr: " ",
+        img: "img/denver.jpg"},
+    {   place: "Salt Lake City, UT",
+        descr: " ",
+        img: "img/utah.jpg"}
+]
