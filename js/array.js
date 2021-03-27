71340c84c8f6d006f9fb40c57d97454547cb3392
Jimmy Ho <jimmyho.929@outlook.com>
2021-03-27 13:36:36 -0400

    successful loop through array

--- js/array.js
+++ js/array.js
@@ -1,12 +1,42 @@
 const places = [
     {   place: "Boston, MA",
-        descr: " ",
+        descr: "asdf ",
         img: "img/boston.jpg"
-        },
+    }
+    , 
     {   place: "Denver, CO",
-        descr: " ",
-        img: "img/denver.jpg"},
+        descr: " asdf",
+        img: "img/denver.png"
+    }
+    ,
     {   place: "Salt Lake City, UT",
-        descr: " ",
-        img: "img/utah.jpg"}
+        descr: " asf",
+        img: "img/utah.jpg"
+    }
 ]
+
+
+// const cityContainerAlernate = document.querySelectorAll(".body");
+
+// for(let i = 0; i<places.length; i++){
+//     cityContainerAlernate[i].innerHTML += `
+//     <img class='city-img' src='${places[i].img}'>
+//         <div class='body-container'>
+//             <p class='city'>${places[i].place}</p>
+//             <p> ${places[i].descr}
+//             </p>
+//         </div>
+//   ` 
+
+// }
+const cityContainer = document.getElementById("city-container");
+    places.forEach( city => { 
+        cityContainer.innerHTML += `<div class='body'>
+        <img class='city-img' src='${city.img}'>
+            <div class='body-container'>
+                <p class='city'>${city.place}</p>
+                <p> ${city.descr}
+                </p>
+            </div>
+        </div>`
+})
